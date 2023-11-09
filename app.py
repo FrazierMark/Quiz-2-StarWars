89742bbf55f98c04d3b43ca3e005299ff0c5cb3c
FrazierMark <m.mark.frazier@gmail.com>
2023-11-09 12:17:29 -0800

    Quiz 2 complete. Stretch challenges complete, able to display homeworld AND films they appear in.

--- app.py
+++ app.py
@@ -1,56 +1,76 @@
 from flask import Flask, request, render_template
 from pprint import PrettyPrinter
 import json
-import os
-import random
 import requests
 
 app = Flask(__name__)
 
 
-API_KEY = os.getenv('API_KEY')
-print(API_KEY)
-
-TENOR_URL = 'https://api.tenor.com/v1/search'
+STARWARS_URL = 'https://swapi.py4e.com/api/'
 pp = PrettyPrinter(indent=4)
+
+starwars_categories = {
+    "films": "https://swapi.py4e.com/api/films/",
+    "people": "https://swapi.py4e.com/api/people/",
+    "planets": "https://swapi.py4e.com/api/planets/",
+    "species": "https://swapi.py4e.com/api/species/",
+    "starships": "https://swapi.py4e.com/api/starships/",
+    "vehicles": "https://swapi.py4e.com/api/vehicles/"
+}
 
 @app.route('/')
 def homepage():
-    """Homepage Route"""
-    return render_template('home.html')
+    """Homepage Route, Show form of Star Wars Categories"""
+    category = request.args.get('category')
+    context = {
+        'starwars_categories': starwars_categories.keys(),
+        'indexes': range(1, 15),
+    }
+    return render_template('home.html', **context)
 
-@app.route('/gif_search', methods=['GET', 'POST'])
-def gif_search():
+
+@app.route('/get_starwars_facts', methods=['GET', 'POST'])
+def starwars_search():
     """Show a form to search for GIFs and show resulting GIFs from Tenor API."""
+        
     if request.method == 'POST':
-        # TODO: Get the search query & number of GIFs requested by the user, store each as a 
-        # variable
+        film_list = []
+        category = request.form.get('category')
+        url = starwars_categories.get(category)
+        index = request.form.get('index')
+        request_url = url+f'{index}/'
+        
+        # Gets all starwars data for a given category and index
+        response = requests.get(request_url)
+        starwars_data = json.loads(response.content)
 
-        response = requests.get(
-            TENOR_URL,
-            {
-                # TODO: Add in key-value pairs for:
-                # - 'q': the search query
-                # - 'key': the API key (defined above)
-                # - 'limit': the number of GIFs requested
-            })
+        
+        # Gets all film titles and homeworld for a given person
+        if category == 'people' and starwars_data:
+            home_world_response = requests.get(starwars_data['homeworld'])
+            home_world = json.loads(home_world_response.content)['name']
+            
+            for film in starwars_data['films']:
+                film_list_response = requests.get(film)
+                film_list.append(json.loads(film_list_response.content)['title'])
+            
 
-        gifs = json.loads(response.content).get('results')
+        if len(film_list) > 0 and home_world != '':
+            context = {
+                'category': category,
+                'film_list': film_list,
+                'home_world': home_world,
+                'starwars_data': starwars_data
+            }
+        else:
+            context = {
+                'category': category,
+                'starwars_data': starwars_data
+            }
 
-        context = {
-            'gifs': gifs
-        }
-
-         # Uncomment me to see the result JSON!
-        # Look closely at the response! It's a list
-        # list of data. The media property contains a 
-        # list of media objects. Get the gif and use it's 
-        # url in your template to display the gif. 
-        # pp.pprint(gifs)
-
-        return render_template('gif_search.html', **context)
+        return render_template('starwars_results.html', **context)
     else:
-        return render_template('gif_search.html')
+        return render_template('home.html')
 
 
 
